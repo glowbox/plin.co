c19ccc9040f8c02087f0fcc90afa460aa19bfe55
John Brown <thisisjohnbrown@gmail.com>
2014-07-17 20:21:12 -0700

    clean up files

--- public/js/app/controllers/board.js
+++
@@ -1,6 +0,0 @@
-var boardController = {
-  init: function () {
-    
-  }
-};
-export { boardController };
--- public/js/dist/app.js
+++ public/js/dist/app.js
@@ -116,65 +116,139 @@ window.app = app;
 })( window );
 (function ( window, document, undefined ) {
   "use strict";
-  /*!
-   *
-   * Welcome to grunt-nautilus
-   * https://github.com/kitajchuk/grunt-nautilus
-   *
-   * Copyright (c) 2013 Brandon Kitajchuk
-   * Licensed under the MIT license.
-   *
-   *
-   * Application Layout:
-   * -----------------------------------------------------------------
-   * Keep all your application files in the app dir of your js root.
-   *      @example: static/js/app/yourmodules...
-   *
-   * Keep all your third party files in the lib dir of your js root.
-   *      @example: static/js/lib/theirmodules...
-   *
-   *
-   * Using Controllers:
-   * -----------------------------------------------------------------
-   * Controllers are how scripts are compiled and built,
-   * that is to say they are the "target" files or starting points
-   * for your application to build from. Controllers build into their
-   * own dist js and can be included as such on your web pages.
-   *      - @example: static/js/app/controllers/foo.js
-   *      - This will compile to static/js/dist/foo.js
-   *      - The compiled script will include all dependencies.
-   *
-   * Controllers assume an init method that can be called via app.exec().
-   * A basic contoller template looks like this:
-   * var fooController = {
-   *       init: function () {
-   *       
-   *       }
-   * };
-   *
-   * export { fooController };
-   *
-   * In the compiled build this script will execute:
-   * app.exec( "fooController" );
-   *
-   *
-   * Using the ES6 syntax for modules:
-   * -----------------------------------------------------------------
-   * This plugin uses the es6-module-transpiler to parse es6 syntax
-   * allowing you to write modular applications with layouts that
-   * make sense. https://github.com/square/es6-module-transpiler.
-   *
-   * Importing Modules
-   *      - @example: import { bar } from "app/foo/bar";
-   *      - @example: import { baz as _baz } from "app/foo/baz";
-   *      - @example: import { foo, bar, baz } from "app/commons";
-   *
-   * Exporting Modules
-   *      - @example: export default = foo;
-   *      - @example: export { foo };
-   *      - @example: export { foo, bar, baz }
-   *
-   *
-   */
-  app.log( "app.js" );
+  var two;
+  var canvas;
+  var context;
+
+  $(function() {
+
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+
+    SHOW_PEGS = true;
+
+    var runCurr = 0;
+    var startTime = new Date().getTime() + 1000;
+
+    if (SHOW_PEGS) {
+      two = new Two({
+        type: Two.Types['svg'],
+        fullscreen: true
+      }).appendTo(document.body);
+
+      Two.Resolution = 10;
+    }
+
+    var board = new Board(SHOW_PEGS);
+    var viz = new Visualization(board, parseInt(puckID.toLowerCase(), 36));
+
+    function animate() {
+      if (runCurr < runStats.length) {
+        if (startTime + runStats[runCurr][0] * 1000 < new Date().getTime()) {
+          viz.hit(runCurr, parseInt(runStats[runCurr][1]));
+          runCurr++;
+        }
+      }
+      requestAnimationFrame(animate);
+    }
+
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
+    
+    resizeCanvas();
+    animate();
+  });
+
+
+
+
+
+
+  function Board(showPegs) {
+    this.pegsHit = [];
+
+    this.numPegs = 78;
+    this.pegRadius = 3;
+    this.BOARD_RATIO = 36/46.8;
+    this.pegHeight = (window.innerHeight - 50);
+    this.pegWidth = this.pegHeight * this.BOARD_RATIO;
+    if (window.innerWidth < this.pegWidth) {
+      this.pegWidth = (window.innerWidth - 50);
+      this.pegHeight = this.pegWidth / this.BOARD_RATIO;
+    }
+    this.pegs = [];
+    this.pegSpacing = this.pegWidth / 6;
+    this.pegOffsetX = (window.innerWidth - this.pegWidth) / 2;
+    this.pegOffsetY = (window.innerHeight - this.pegHeight) / 2;
+
+    this.init = function() {
+      if (showPegs) {
+        two = new Two({
+          type: Two.Types['svg'],
+          fullscreen: true
+        }).appendTo(document.body);
+
+        Two.Resolution = 10;
+      }
+      for (var i = 0; i < this.numPegs; i++) {
+        var localPin = i % 13;
+        var overallWidth = 13 * this.pegSpacing;
+        var x = 0;
+        var y = Math.floor(i / 13) * (this.pegSpacing * .866666667 * 2) + this.pegOffsetY + (this.pegSpacing * .8666);
+        if (localPin <= 6) {
+          var num = i;
+          if (i === 65) {
+            num = 0;
+          }
+          x = (num /13 * overallWidth) % overallWidth + this.pegOffsetX + this.pegSpacing/2;
+          y -= this.pegSpacing * .866666667;
+        } else {
+          x = (i /13 * overallWidth) % overallWidth - (overallWidth / 2)  + this.pegOffsetX + this.pegSpacing/2;
+        }
+
+        if (showPegs) {
+          var peg = two.makeCircle(x, y, this.pegRadius);
+          peg.fill = 'rgba(255, 255, 255, ' + i/this.numPegs + ')';
+          two.update();
+          $(peg._renderer.elem)
+            .css('cursor', 'pointer')
+            .click(function(e) {
+              var pegNum = $(this).index();
+              pegHit(pegNum);
+            });
+        }
+        this.pegs.push({x: x, y: y});
+      }
+    }
+
+    this.getPinCoordinates = function(index) {
+      return {
+        x: this.pegs[index].x + this.pegOffsetX,
+        y: this.pegs[index].y + this.pegOffsetY
+      }
+    }
+    this.init();
+  }
+
+
+
+
+
+
+  function Visualization(board, puckID) {
+    this.board = board;
+    this.puckID = puckID;
+    this.hit = function(runCurr, index) {
+      var coor = board.getPinCoordinates(index);
+      context.beginPath();
+      context.arc(coor.x, coor.y, 20, 0, 2 * Math.PI, false);
+      context.fillStyle = 'rgb(' +
+        Math.floor((this.puckID * (runCurr + 1) * 17)) % 255 + ',' +
+        Math.floor((this.puckID * (runCurr + 1) * 25)) % 255 + ',' +
+        Math.floor((this.puckID * (runCurr + 1) * 40)) % 255 + ')';
+      context.fill();
+    }
+  }
 })( window, window.document );
