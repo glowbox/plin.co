206882bf4648a5133b3103c1bb292855af52452f
John Brown <thisisjohnbrown@gmail.com>
2014-11-17 00:05:26 -0600

    Break out init functions

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -45,39 +45,7 @@ $(function() {
 
   if (live) {
     socket = io('http://localhost');
-    socket.on('connect', function(){
-      if (DEBUG) console.log('connected!');
-      socket.on('peg', function(data){
-        if (DEBUG) console.log(data.index);
-        if (lastPeg !== parseInt(data.index, 10)) {
-          lastPeg = parseInt(data.index, 10);
-          if (!hasStarted) {
-            hasStarted = true;
-            startTime = new Date().getTime();
-            if (DEBUG) console.log('reset start Time');
-          }
-          viz.hit(0, parseInt(data.index, 10));
-        }
-      });
-      socket.on('start', function(data){
-        if (DEBUG) console.log('RUNNING ID: ' + data.id);
-      });
-      socket.on('reset', function(data){
-        freeMode = data.freemode;
-        puckID = data.id;
-        viz.destroy();
-        viz = chooseViz(puckID);
-        resizeCanvas();
-        hasStarted = false;
-        // $.post('/play');
-      });
-      socket.on('end', function(data){
-        puckID = data.id;
-        hasStarted = false;
-        if (DEBUG) console.log('NEXT ID: ' + data.id);
-      });
-      socket.on('disconnect', function(){});
-    });
+    socket.on('connect', appSocketOnConnect);
   }
 
   camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
@@ -118,172 +86,213 @@ $(function() {
   viz = chooseViz(puckID);
 
   // viz = new ParticleEsplode(board, parseInt(puckID.toLowerCase(), 36));
-
-  worker = new Worker( '/js/app/worker.js' );
-  worker.onmessage = function( event ){
-    console.log( event.data );
-  };
-
-  function animate() {
-    var now = new Date().getTime();
-    if (live && hasStarted && !freeMode) {
-      var diffMain = now - startTime;
-      if (diffMain > 0 && diffMain < viz.gifLength) {
-        var diffLast = now - lastImageTime;
-        if (diffLast > 1000 / viz.framesPerSecond) {
-          lastImageTime = now;
-          var tempCanvas = document.createElement("canvas"),
-              tempCtx = tempCanvas.getContext("2d");
-
-          var dOffset = ((viz.double && window.devicePixelRatio) > 1 ? 2 : 1);
-          var maxWidth = (board.boardWidth) * dOffset;
-          var maxHeight = (board.boardHeight) * dOffset;
-          var ratio = 37/58;
-          tempCanvas.width = canvas.width;
-          tempCanvas.height = canvas.height;
-          var sizeRatio = maxWidth / tempCanvas.width;
-
-
-          gifSize = {
-            'width': tempCanvas.width,
-            'height': tempCanvas.height
-          }
-
-          tempCtx.fillStyle = "black";
-          tempCtx.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
-          tempCtx.drawImage(canvas, 0, 0);
-          // tempCanvas.width = 480;
-          // tempCanvas.height = Math.floor(480 * ratio);
-          contexts.push(tempCanvas);
-          // if (viz.double) {
-          //   var imageData = tempCtx.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
-          //   var data = imageData.data;
-
-          //   for(var i = 0; i < data.length; i += 4) {
-          //     // red
-          //     data[i] = 255 - data[i];
-          //     // green
-          //     data[i + 1] = 255 - data[i + 1];
-          //     // blue
-          //     data[i + 2] = 255 - data[i + 2];
-          //   }
-          //   tempCtx.putImageData(imageData, 0, 0);
-          // }
-          gifs.push(0);
-        }
-      } else if (diffMain > viz.gifLength && gifs.length) {
-        uploadImages();
-        if (DEBUG) console.log('done');
-        gifs = [];
-      }
-    }
-    if (runCurr < runStats.length) {
-      if (startTime + runStats[runCurr][0] * 1000 < now) {
-        viz.hit(runCurr, parseInt(runStats[runCurr][1]));
-        runCurr++;
-      }
-    }
-    viz.render();
-
-    // draw pegs on canvas to help alignment.
-    if (CALIBRATE_MAPPING) {
-     context.fillStyle = "white";
-     context.strokeStyle = "white";
-     context.strokeRect(0,0,canvas.width,canvas.height);
-     context.strokeRect(1,1,canvas.width-2,canvas.height-2);
-     for(var i = 0; i < board.numPegs; i++){
-       var coords = board.getPinCoordinates(i);
-        context.fillRect(coords.x-2, coords.y-2, 5, 5);
-     }
-    }
-
-
-    renderer.render( scene, camera );
-    requestAnimationFrame(animate);
-  }
-
-  function resizeCanvas() {
-    canvas.width = isLive ? SCREEN_WIDTH : window.innerWidth;
-    canvas.height = isLive ? SCREEN_HEIGHT : window.innerHeight;
-    if (viz.double && !isLive) {
-      canvas.width = canvas.width * ((viz.double && window.devicePixelRatio) > 1 ? 2 : 1);
-      canvas.height = canvas.height * ((viz.double && window.devicePixelRatio) > 1 ? 2 : 1);
-    }
-  }
   
   resizeCanvas();
   animate();
 
-  $(window).bind('keydown', function(e) {
-    if (e.keyCode === 32) {
-      if(CALIBRATE_MAPPING){
-        $.post('/set-projection-points/', {'targetPoints': JSON.stringify(targetPoints)});
-      }
-      CALIBRATE_MAPPING = !CALIBRATE_MAPPING;
-    }
-    if(CALIBRATE_MAPPING){
-      var index = -1;
-      switch(e.keyCode) {
-        case 49: // 1
-          index = 0;
-        break;
-        
-        case 50: // 2
-         index = 1;
-        break;
-        
-        case 81: // q
-          index = 3;
-        break;
-        
-        case 87: // w
-          index = 2;
-        break;
-
-        default:
-          console.log(e.keyCode);
-      }
-      if(index !== -1) {
-        targetPoints[index][0] = mousePosition.x;
-        targetPoints[index][1] = mousePosition.y;
-        updatePerspectiveTransform();
-      }
-    }
-  });
-
-  $(window).bind('mousedown', function(e) {
-    
-    if(CALIBRATE_MAPPING){
-      var mx = e.pageX;
-      var my = e.pageY;
-
-      for(var i = 0; i < 4; i++){
-        if(distanceTo(mx, my, targetPoints[i][0], targetPoints[i][1]) < 60){
-          dragging = true;
-          dragIndex = i;
-          break;
-        }
-      }
-    }
-  });
-
-  $(window).bind('mouseup', function(e) {
-    if(dragging){
-    }
-    dragging = false;
-  });
-
-  $(window).bind('mousemove', function(e) {
-    if(dragging && CALIBRATE_MAPPING) {
-      targetPoints[dragIndex][0] = e.pageX;
-      targetPoints[dragIndex][1] = e.pageY;
-      updatePerspectiveTransform();
-    }
-    mousePosition.x = e.pageX;
-    mousePosition.y = e.pageY;
-  });
+  $(window).bind('keydown', appKeyDown);
+  $(window).bind('mousedown', appMouseDown);
+  $(window).bind('mouseup', appMouseUp);
+  $(window).bind('mousemove', appMouseMove);
   // updatePerspectiveTransform();
 });
+
+function appSocketOnConnect(){
+  if (DEBUG) console.log('connected!');
+  socket.on('peg', appSocketOnPeg);
+  socket.on('start', appSocketOnStart);
+  socket.on('reset', appSocketOnReset);
+  socket.on('end', appSocketOnEnd);
+  socket.on('disconnect', function(){});
+}
+
+function appSocketOnEnd(data){
+  puckID = data.id;
+  hasStarted = false;
+  if (DEBUG) console.log('NEXT ID: ' + data.id);
+}
+
+function appSocketOnReset(data){
+  freeMode = data.freemode;
+  puckID = data.id;
+  viz.destroy();
+  viz = chooseViz(puckID);
+  resizeCanvas();
+  hasStarted = false;
+  // $.post('/play');
+}
+
+function appSocketOnStart(data){
+  if (DEBUG) console.log('RUNNING ID: ' + data.id);
+}
+
+function appSocketOnPeg(data){
+        if (DEBUG) console.log(data.index);
+        if (lastPeg !== parseInt(data.index, 10)) {
+          lastPeg = parseInt(data.index, 10);
+          if (!hasStarted) {
+            hasStarted = true;
+            startTime = new Date().getTime();
+            if (DEBUG) console.log('reset start Time');
+          }
+          viz.hit(0, parseInt(data.index, 10));
+        }
+      }
+
+function appKeyDown(e) {
+  if (e.keyCode === 32) {
+    if(CALIBRATE_MAPPING){
+      $.post('/set-projection-points/', {'targetPoints': JSON.stringify(targetPoints)});
+    }
+    CALIBRATE_MAPPING = !CALIBRATE_MAPPING;
+  }
+  if(CALIBRATE_MAPPING){
+    var index = -1;
+    switch(e.keyCode) {
+      case 49: // 1
+        index = 0;
+      break;
+      
+      case 50: // 2
+       index = 1;
+      break;
+      
+      case 81: // q
+        index = 3;
+      break;
+      
+      case 87: // w
+        index = 2;
+      break;
+
+      default:
+        console.log(e.keyCode);
+    }
+    if(index !== -1) {
+      targetPoints[index][0] = mousePosition.x;
+      targetPoints[index][1] = mousePosition.y;
+      updatePerspectiveTransform();
+    }
+  }
+}
+
+function appMouseMove(e) {
+  if(dragging && CALIBRATE_MAPPING) {
+    targetPoints[dragIndex][0] = e.pageX;
+    targetPoints[dragIndex][1] = e.pageY;
+    updatePerspectiveTransform();
+  }
+  mousePosition.x = e.pageX;
+  mousePosition.y = e.pageY;
+}
+
+function appMouseUp(e) {
+  if(dragging){
+  }
+  dragging = false;
+}
+
+function appMouseDown(e) {    
+  if(CALIBRATE_MAPPING){
+    var mx = e.pageX;
+    var my = e.pageY;
+
+    for(var i = 0; i < 4; i++){
+      if(distanceTo(mx, my, targetPoints[i][0], targetPoints[i][1]) < 60){
+        dragging = true;
+        dragIndex = i;
+        break;
+      }
+    }
+  }
+}
+
+function animate() {
+  var now = new Date().getTime();
+  if (live && hasStarted && !freeMode) {
+    var diffMain = now - startTime;
+    if (diffMain > 0 && diffMain < viz.gifLength) {
+      var diffLast = now - lastImageTime;
+      if (diffLast > 1000 / viz.framesPerSecond) {
+        lastImageTime = now;
+        var tempCanvas = document.createElement("canvas"),
+            tempCtx = tempCanvas.getContext("2d");
+
+        var dOffset = ((viz.double && window.devicePixelRatio) > 1 ? 2 : 1);
+        var maxWidth = (board.boardWidth) * dOffset;
+        var maxHeight = (board.boardHeight) * dOffset;
+        var ratio = 37/58;
+        tempCanvas.width = canvas.width;
+        tempCanvas.height = canvas.height;
+        var sizeRatio = maxWidth / tempCanvas.width;
+
+
+        gifSize = {
+          'width': tempCanvas.width,
+          'height': tempCanvas.height
+        }
+
+        tempCtx.fillStyle = "black";
+        tempCtx.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
+        tempCtx.drawImage(canvas, 0, 0);
+        // tempCanvas.width = 480;
+        // tempCanvas.height = Math.floor(480 * ratio);
+        contexts.push(tempCanvas);
+        // if (viz.double) {
+        //   var imageData = tempCtx.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
+        //   var data = imageData.data;
+
+        //   for(var i = 0; i < data.length; i += 4) {
+        //     // red
+        //     data[i] = 255 - data[i];
+        //     // green
+        //     data[i + 1] = 255 - data[i + 1];
+        //     // blue
+        //     data[i + 2] = 255 - data[i + 2];
+        //   }
+        //   tempCtx.putImageData(imageData, 0, 0);
+        // }
+        gifs.push(0);
+      }
+    } else if (diffMain > viz.gifLength && gifs.length) {
+      uploadImages();
+      if (DEBUG) console.log('done');
+      gifs = [];
+    }
+  }
+  if (runCurr < runStats.length) {
+    if (startTime + runStats[runCurr][0] * 1000 < now) {
+      viz.hit(runCurr, parseInt(runStats[runCurr][1]));
+      runCurr++;
+    }
+  }
+  viz.render();
+
+  // draw pegs on canvas to help alignment.
+  if (CALIBRATE_MAPPING) {
+   context.fillStyle = "white";
+   context.strokeStyle = "white";
+   context.strokeRect(0,0,canvas.width,canvas.height);
+   context.strokeRect(1,1,canvas.width-2,canvas.height-2);
+   for(var i = 0; i < board.numPegs; i++){
+     var coords = board.getPinCoordinates(i);
+      context.fillRect(coords.x-2, coords.y-2, 5, 5);
+   }
+  }
+
+
+  renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+}
+
+function resizeCanvas() {
+  canvas.width = isLive ? SCREEN_WIDTH : window.innerWidth;
+  canvas.height = isLive ? SCREEN_HEIGHT : window.innerHeight;
+  if (viz.double && !isLive) {
+    canvas.width = canvas.width * ((viz.double && window.devicePixelRatio) > 1 ? 2 : 1);
+    canvas.height = canvas.height * ((viz.double && window.devicePixelRatio) > 1 ? 2 : 1);
+  }
+}
 
 function distanceTo(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
