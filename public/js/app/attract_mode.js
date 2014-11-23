30f603e9470f7337d3dc41d5b4f4a0aea01c751c
Ben Purdy <ben.purdy@gmail.com>
2014-11-22 20:44:53 -0800

    Attract mode is more attractive.

--- public/js/app/attract_mode.js
+++ public/js/app/attract_mode.js
@@ -39,6 +39,40 @@ function AttractMode(board, puckID) {
   }
 
 
+  this.letters = {
+    "p" : {
+      pins : [26, 20, 14, 8, 15, 21, 20],
+      index : 0,
+      color: hexToRgb(colorValues[0])
+    },
+    "l" : {
+      pins : [28, 22, 16, 10],
+      index : 0,
+      color: hexToRgb(colorValues[1])
+    },
+    "i" : {
+      pins : [29, 23],
+      index : 0,
+      color: hexToRgb(colorValues[2])
+    },
+    "n" : {
+      pins : [30, 24, 31, 25, 19],
+      index : 0,
+      color: hexToRgb(colorValues[3])
+    }, 
+    "c" : {
+      pins : [47, 46, 40, 34, 35],
+      index : 0,
+      color: hexToRgb(colorValues[4])
+    },
+    "o" : {
+      pins : [48, 42, 36, 37, 43, 49, 48],
+      index : 0,
+      color: hexToRgb(colorValues[5])
+    }
+  };
+
+
   for(var i = 0; i < board.pegs.length; i++){
     this.pegRanges.push( Math.random() * 8 + 3 );
   }
@@ -83,7 +117,7 @@ function AttractMode(board, puckID) {
           
           context.beginPath();
           context.lineWidth = this.particles[i].lineWidth * influence + 0.01;
-          context.strokeStyle = "rgba(" + this.particles[i].color.join(",") + ", " + (influence) * 0.75 + ")";
+          context.strokeStyle = "rgba(" + this.particles[i].color.join(",") + ", " + (influence) * 0.175 + ")";
           context.moveTo(this.particles[i].x, this.particles[i].y);
           context.lineTo(board.pegs[p].x, board.pegs[p].y);
           context.stroke();
@@ -97,6 +131,42 @@ function AttractMode(board, puckID) {
 
       this.particles[i].vx *= 0.98;
       this.particles[i].vy *= 0.97;
+    }
+
+    for(var ltr in this.letters) {
+      context.lineWidth = 0.1;
+      var letter = this.letters[ltr];
+      context.strokeStyle = "rgba(" + letter.color.r + ", " + letter.color.g + ", " + letter.color.b + ", 0.5)";
+      context.globalCompositeOperation = 'lighter';
+      context.beginPath();
+      
+      var coord = board.getPinCoordinates(letter.pins[0]);
+      
+      for(var i = 0; i < letter.pins.length; i++) {
+        coord = board.getPinCoordinates(letter.pins[i]);
+
+        for(var p = 0; p < this.particles.length; p++) {
+          var d = distanceTo(this.particles[p].x, this.particles[p].y, coord.x, coord.y);
+          
+          if(d < 3) {
+            
+            var amt = 1 - (d / 3);
+            context.lineWidth = 0.5 * amt;
+            coord.x -= (coord.x - this.particles[p].x) * amt;
+            coord.y -= (coord.y - this.particles[p].y) * amt;
+          }
+        }
+        if(i==0){
+          context.moveTo(coord.x+Math.random() * 0.25, coord.y+Math.random() * 0.25);
+        } else {
+          context.lineTo(coord.x+Math.random() * 0.25, coord.y+Math.random() * 0.25);
+        }
+      }
+      context.stroke();
+      //letter.index += 0.25;
+      //if(letter.index > letter.pins.length){
+      //  letter.index = 0;
+     // }
     }
   }
 
