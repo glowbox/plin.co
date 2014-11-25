4236d0965cd2a6630ded4e6e0286c1d2e9cf6deb
John Brown <thisisjohnbrown@gmail.com>
2014-11-24 21:53:17 -0800

    fix frame rate issue with Warp

--- public/js/app/strum.js
+++ public/js/app/strum.js
@@ -29,24 +29,85 @@ function StrumViz(board, puckID) {
   this.render = function(context, deltaTime) {
     this.context = context;
 
-    this.context.fillStyle = 'rgba(0, 0, 0, .1)';
-    this.context.fillRect(0, 0, this.board.width, this.board.height);
-    for (var i = 0; i < this.strums.length; i++) {
-      this.drawLines(this.strums[i]);
-      this.strums[i].tick += (this.strums[i].speed / 10) * deltaTime;
+
+    // var now = performance.now();
+ 
+    // delay = (now - last) * 0.05 + delay * 0.95;
+    // last = now;
+   
+    this.context.clearRect(0, 0, canvas.width, canvas.height);
+    // this.context.fillText(~~delay +'ms / ' +  (1000/ delay | 0) + 'fps', 50, 50);
+    this.context.strokeStyle="white";
+   
+    LINES = 2000;
+    LINE_WIDTH = .1;
+    this.context.lineWidth = LINE_WIDTH;
+    
+    for (i=0; i < LINES; i++) {
+   
+      // If test lines  
+      // this.context.beginPath();
+      // this.context.moveTo(Math.random() * canvas.width * 0.8, Math.random() * canvas.height * 0.8);
+      // this.context.lineTo(Math.random() * canvas.width * 0.8, Math.random() * canvas.height * 0.8);
+      // this.context.stroke();
+   
+      // If test curves
+      // this.context.beginPath();
+      // this.context.moveTo(50 + Math.random() * innerWidth * 0.8, 50 + Math.random() * innerHeight * 0.8);
+      // this.context.quadraticCurveTo(
+      //   50 + Math.random() * innerWidth * 0.8, 50 + Math.random() * innerHeight * 0.8,
+      //   50 + Math.random() * innerWidth * 0.8, 50 + Math.random() * innerHeight * 0.8);
+      // this.context.stroke();
+   
     }
-    // this.drawLines(20, hexToRgb(colors[1]), 48928);
-    // this.drawLines(20, hexToRgb(colors[2]), 38928);
-    // this.drawLines(20, hexToRgb(colors[3]), 38928);
-    // this.drawLines(20, hexToRgb(colors[4]), 38928);
-    // this.drawLines(20, hexToRgb(colors[5]), 38928);
-    // this.drawLines(100, hexToRgb(colors[7]), 12930);
+
+
+    
+
+    // this.context.fillStyle = 'rgba(0, 0, 0, .1)';
+    // this.context.fillRect(0, 0, this.board.width, this.board.height);
+    var cx = this.board.width / 2;
+    var cy = this.board.height / 2;
+    var h = this.board.height;
+    for (var i = 0; i < this.strums.length; i++) {
+      // this.drawLines(this.strums[i]);
+      this.strums[i].tick += (this.strums[i].speed / 10) * deltaTime;
+      for (var j = 0; j < this.strums[i].num; j++) {
+        var b = this.strums[i].tick + this.strums[i].offset;
+        var a=((Math.sin(b*0.1+i*0.04)+1)*127.5)
+        this.context.strokeStyle = 'rgba(' + this.strums[i].color.r + ', ' + this.strums[i].color.g + ', ' + this.strums[i].color.b + ', ' + 1 + ')';
+        this.context.beginPath();
+        // this.context.moveTo(this.strums[i].coor.x, this.strums[i].coor.y);
+        // var dir = Math.random();
+        // if (dir < .25) {
+        //   this.context.lineTo(0, Math.random() * this.board.height);
+        // } else if (dir < .5) {
+        //   this.context.lineTo(canvas.width, Math.random() * canvas.height);
+        // } else if (dir < .75) {
+        //   this.context.lineTo(Math.random() * canvas.width, 0);
+        // } else {
+        //   this.context.lineTo(Math.random() * canvas.width, canvas.height);
+        // }
+        // this.context.lineTo(0, 0);
+        var x=(Math.sin(j*0.01+b*0.02)*Math.sin(j*0.003+b*0.004)*this.board.width);
+        this.context.beginPath();
+        this.context.moveTo(cx+x,0);
+        this.context.lineTo(cx+x,h);
+        this.context.stroke();
+        this.context.beginPath();
+        this.context.moveTo(cx-x,0);
+        this.context.lineTo(cx-x,h);
+        this.context.stroke();
+        
+
+      }
+    }
   }
 
   this.hit = function(runCurr, index) {
     var coor = this.board.getPinCoordinates(index);
     this.strums.push({
-      'num': this.strums.length ? 1 : 30,
+      'num': this.strums.length ? 10 : 300,
       'color': hexToRgb(this.colorValues[Math.floor(this.colorValues.length * Math.random())]),
       'offset': 0, //Math.floor(Math.random() * 100000),
       'direction': Math.random() < .5 ? 1 : 0,
@@ -59,19 +120,26 @@ function StrumViz(board, puckID) {
   this.drawLines = function(params) {
     var cx = this.board.width / 2;
     var cy = this.board.height / 2;
+    this.context.lineWidth = .1;
+    this.context.strokeStyle = 'white';
     for (var i = 0; i < params.num; i++) {
-      var b = params.tick + params.offset;
-      var a=((Math.sin(b*0.1+i*0.04)+1)*127.5)
+      // var b = params.tick + params.offset;
+      // var a=((Math.sin(b*0.1+i*0.04)+1)*127.5)
       
-      this.context.lineWidth = .1;
-      this.context.strokeStyle = 'rgba(' + params.color.r + ', ' + params.color.g + ', ' + params.color.b + ', ' + Math.floor(a) + ')';
-      this.context.beginPath();
+      // this.context.lineWidth = .1;
+      // this.context.strokeStyle = 'rgba(' + params.color.r + ', ' + params.color.g + ', ' + params.color.b + ', ' + Math.floor(a) + ')';
+      // this.context.beginPath();
 
-      var x=(Math.sin(i*0.01+b*0.02)*Math.sin(i*0.003+b*0.004)*this.board.width);
-      this.context.moveTo(cx+x,0);
-      this.context.lineTo(cx+x,this.board.height);
-      this.context.moveTo(cx-x,0);
-      this.context.lineTo(cx-x,this.board.height);
+      // var x=(Math.sin(i*0.01+b*0.02)*Math.sin(i*0.003+b*0.004)*this.board.width);
+      // this.context.moveTo(cx+x,0);
+      // this.context.lineTo(cx+x,this.board.height);
+      // this.context.moveTo(cx-x,0);
+      // this.context.lineTo(cx-x,this.board.height);
+
+      // this.context.beginPath();
+      // this.context.moveTo(Math.random() * canvas.width * 0.8, Math.random() * canvas.height * 0.8);
+      // this.context.lineTo(Math.random() * canvas.width * 0.8, Math.random() * canvas.height * 0.8);
+      // this.context.stroke();
 
       // var x=(Math.sin(i*0.01+b*0.02)*Math.sin(i*0.003+b*0.004)*this.board.width);
       // this.context.moveTo(params.coor.x+x,0);
@@ -91,3 +159,6 @@ function StrumViz(board, puckID) {
 
   this.init();
 }
+
+var last = performance.now();
+var delay = 0;
