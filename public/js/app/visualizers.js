dbaf78ee4edb1bd8fc8a31eaacaec616c8fed8cc
John Brown <thisisjohnbrown@gmail.com>
2015-02-01 11:34:16 -0800

    Add warp

---
+++ public/js/app/visualizers.js
@@ -0,0 +1,22 @@
+{
+  'attract-mode' :  {
+    'name' : 'Attract',
+    'constructor' : AttractMode
+  },
+  'birds' : {
+    'name' : 'Birds',
+    'constructor' : BirdsViz
+  },
+  'voronoi' :  {
+    'name' : 'Voronoi',
+    'constructor' : VoronoiViz
+  },
+  'particles' :  {
+    'name' : 'Particles',
+    'constructor' : ParticleEsplode
+  },
+  'cardinal' :  {
+    'name' : 'Cardinals',
+    'constructor' : CardinalViz
+  }
+}
